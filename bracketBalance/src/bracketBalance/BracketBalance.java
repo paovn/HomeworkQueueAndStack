fa5332ec4e60143222cfc0d7328b04ab5c248faf
plndrvlznrnj <paola.a.veloza@gmail.com>
2019-02-20 11:29:35 -0500

    NumberSmall implement

--- bracketBalance/src/bracketBalance/BracketBalance.java
+++ bracketBalance/src/bracketBalance/BracketBalance.java
@@ -2,9 +2,14 @@ package bracketBalance;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -12,50 +17,55 @@ public class BracketBalance {
 	
 	public static void main(String[] args) throws IOException {
 		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new FileReader(new File("inputBracketBalance.txt")));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("outputBreacketBalance.lol")));
+		String s ="";
 		
-		String s = bf.readLine();
-		Stack<Character> p = new Stack<>();
-		
-		char[] c = s.toCharArray();
-		try {
-			for (int i = 0; i < c.length; i++) {
-				if (c[i] == '(') {
-					p.push(c[i]);
-				}else if(c[i]=='{') {
-					p.push(c[i]);
-				}else if(c[i]== '[') {
-					p.push(c[i]);
-				}else if(c[i]== ')') {
-					if(p.peek() == '(') {
-						p.pop();	
-					}else {
-						break;
-					}
-				}else if(c[i]=='}') {
-					if(p.peek()=='{') {
-						p.pop();
-					}else {
-						break;
-					}
-				}else if(c[i]==']') {
-					if(p.peek()=='[') {
-						p.pop();
-					}else {
-						break;
+		while((s=br.readLine())!=null) {
+			Stack<Character> p = new Stack<>();
+			
+			char[] c = s.toCharArray();
+			
+				for (int i = 0; i < c.length; i++) {
+					if (c[i] == '(') {
+						p.push(c[i]);
+					}else if(c[i]=='{') {
+						p.push(c[i]);
+					}else if(c[i]== '[') {
+						p.push(c[i]);
+					}else if(c[i]== ')') {
+						if(p.peek() == '(') {
+							p.pop();	
+						}else {
+							break;
+						}
+					}else if(c[i]=='}') {
+						if(p.peek()=='{') {
+							p.pop();
+						}else {
+							break;
+						}
+					}else if(c[i]==']') {
+						if(p.peek()=='[') {
+							p.pop();
+						}else {
+							break;
+						}
 					}
 				}
-			}
-			if(p.isEmpty()) {
-				System.out.println("true");
-			}else {
-				System.out.println("false");
-			}
-		}catch(Exception e ){
-			System.out.println("false");
-			e.printStackTrace();
+				
+				if(p.isEmpty()) {
+					bw.write("true");
+					bw.flush();
+				}else {
+					bw.write("false");
+					bw.flush();
+				}
+				
 			
 		}
+		br.close();
+		bw.close();
 		
 	
 	}
--- bracketBalance/src/numberSmall/NumberSmall.java
+++ bracketBalance/src/numberSmall/NumberSmall.java
@@ -3,19 +3,46 @@ package numberSmall;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class NumberSmall {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String ...args) throws IOException {
+		System.out.println("Indique el numero de veces que quiere que se elimine");
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int k = Integer.parseInt(bf.readLine());
 		String s = bf.readLine();
 		Stack<Integer> p = new Stack<>();
-		
-		for (int i = 0; i < args.length; i++) {
-			
+		Queue<Integer> q = new LinkedList<Integer>();
+		String[] numbers = s.split(" ");
+		for (int i = 0; i < numbers.length; i++) {
+			q.add(Integer.parseInt(numbers[i]));
 		}
+		p.push(q.poll());
+		while (k > 0 && !q.isEmpty()) {
+			Integer num = q.poll();
+			if (num > q.peek()) {
+				while (k > 0  && !p.isEmpty() && num > p.peek()) {
+					p.pop();	
+					k--;
+				}
+				p.push(num);
+			}else {
+				k--;
+			}
+		}
+		String toPrint = "";
+		for (Integer num : p) {
+			toPrint = num + " " + toPrint;
+		}
+		
+		for (Integer num : q)     {
+			toPrint += " "+ num;
+		}
+		
+		System.out.println(toPrint);
 
 	}
 
